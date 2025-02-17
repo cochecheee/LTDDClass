7266b01670bf48061586fe3597aa74f404be5658
tien.blt <buitien747@gmail.com>
2025-02-17 17:47:09 +0700

    add spring secur and fix encrypt pass error

---
+++ ExerciseW3/BT2_SECURITY/src/main/java/com/example/bt2/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.example.bt2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+	@Bean
+	UserDetailsService userDetailsService(PasswordEncoder encoder) {
+		UserDetails admin = User.withUsername("tienbui")
+				.password(passwordEncoder().encode("123456")) // fix encrypt pass
+				.roles("ADMIN") 
+				.build();
+		
+		UserDetails user = User.withUsername("cocheche")
+								.password(passwordEncoder().encode("123456"))
+								.roles("USER")
+								.build();
+		
+		return new InMemoryUserDetailsManager(admin, user);
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	// filter
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http.csrf(csrf -> csrf.disable())
+				.authorizeHttpRequests(auth -> auth
+						.requestMatchers("/").permitAll()
+						.requestMatchers("/customer/**").authenticated()
+						)
+				.formLogin(Customizer.withDefaults())
+				.build();
+	}
+}
