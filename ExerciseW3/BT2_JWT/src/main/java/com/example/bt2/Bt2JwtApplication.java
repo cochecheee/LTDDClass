9ce318dc8b6ce2222a0ee55c3031c1fb48bad24b
tien.blt <buitien747@gmail.com>
2025-02-17 17:39:35 +0700

    add security for app

---
+++ ExerciseW3/BT2_JWT/src/main/java/com/example/bt2/Bt2JwtApplication.java
@@ -0,0 +1,13 @@
+package com.example.bt2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bt2JwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bt2JwtApplication.class, args);
+	}
+
+}
